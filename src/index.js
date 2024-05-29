b531a8eb09b6af2c08cff00b2c5b5939fd111361
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-29 18:58:17 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -6,35 +6,36 @@ const books = require("./routes/books");
 require("dotenv").config();
 
 const app = express();
+
 const port = process.env.PORT || 3000;
-
-// MongoDB connection
-const mongoUri = process.env.MONGO_URI;
-if (!mongoUri) {
-  console.error("MongoDB URI is not defined in the environment variables.");
-  process.exit(1);
-}
-
-mongoose
-  .connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.use(
+  cors({
+    credentials: true,
   })
-  .catch((error) => console.error("MongoDB connection error:", error));
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-app.use(cors());
+);
 app.use(bodyParser.json());
 
-app.use("/api/books", books);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use("/api/books", books);
+// MongoDB connection
+mongoose
+  .connect(
+    process.env.MONGO_URI ?? "mongodb://localhost:27017/book-api"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 module.exports = app; // Export for Vercel
