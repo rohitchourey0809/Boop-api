3bd229f051442cd1b4ea4d0608276075e57b7470
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-29 18:36:30 +0530

    fixed versel

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const books = require("./routes/book");
+const books = require("./routes/books");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // MongoDB connection
-// MongoDB connection
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
-  console.error('MongoDB URI is not defined in the environment variables.');
+  console.error("MongoDB URI is not defined in the environment variables.");
   process.exit(1);
 }
 
-mongoose.connect(mongoUri);
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -32,3 +36,5 @@ app.use("/api/books", books);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app; // Export for Vercel
